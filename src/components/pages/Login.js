4f5d7b9fe48bf155a0f84e88e419e1861f7d793a
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-01 12:26:32 +0600

    login and sign up done

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,23 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc'
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import Loading from '../utilities/Loading';
+import { toast } from 'react-toastify';
 
 const Login = () => {
-    const handleLoginForm = (e) => {
+    const toastConfig = {
+        position: "top-center",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    }
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const [signInWithGoogle, user1, loading1, error1] = useSignInWithGoogle(auth);
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(
+        auth
+    );
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const handleLoginForm = async (e) => {
         e.preventDefault();
+        signInWithEmailAndPassword(email, password)
+    }
+    const handleReset = async (e) => {
+        if (!email) {
+            toast.error('Please enter your email', toastConfig)
+            return
+        }
+        await sendPasswordResetEmail(email)
+        toast.info('Sent an password reset mail to given email address', toastConfig)
+    }
+    useEffect(() => {
+        if (error?.message) {
+            toast.error(error.message, toastConfig)
+        }
+        else if (error1?.message) {
+            toast.error(error.message, toastConfig)
+        }
+    }, [error, error1])
+    useEffect(() => {
+        if (user || user1) {
+
+        }
+    }, [user, user1])
+    if (loading || loading1 || sending) {
+        return <Loading></Loading>
     }
     return (
         <div style={{ minHeight: '600px' }} className="flex justify-center items-center">
             <div className="w-full md:w-1/2">
                 <h1 className="text-3xl text-center mb-3">Login</h1>
                 <form onSubmit={handleLoginForm}>
-                    <input type="email" name="email" className='bg-gray-200 p-3 w-3/4 block mx-auto' placeholder="Email" /> <br />
-                    <input type="password" name="password" className='bg-gray-200 p-3 my-2 w-3/4 block mx-auto' placeholder="Password" /> <br />
+                    <input type="email" name="email" className='bg-gray-200 p-3 w-3/4 block mx-auto' placeholder="Email" onChange={(e) => setEmail(e.target.value)} /> <br />
+                    <input type="password" name="password" className='bg-gray-200 p-3 my-2 w-3/4 block mx-auto' placeholder="Password" onChange={(e) => setPassword(e.target.value)} /> <br />
                     <button type="submit" className="bg-blue-500 w-1/2 mx-auto block p-3">Login</button>
                 </form>
+                <button className="block mx-auto text-red-500 my-3" onClick={handleReset}>Forget Password?</button>
                 <p className="text-center">or</p>
                 <p className="text-center my-2">New user? <Link to="/register" className='text-green-500'>Register Now</Link></p>
-                <button className="bg-blue-500 w-1/2 mx-auto block p-3">
+                <button className="bg-blue-500 w-1/2 mx-auto block p-3" onClick={() => signInWithGoogle()}>
                     <FcGoogle className="inline mx-2"></FcGoogle>
                     <span>Continue With Google</span>
                 </button>
--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc'
 import { toast } from 'react-toastify';
-import { useCreateUserWithEmailAndPassword, useSendEmailVerification, useUpdateProfile } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSendEmailVerification, useSignInWithGoogle, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import Loading from '../utilities/Loading';
 
@@ -17,13 +17,14 @@ const Register = () => {
         progress: undefined,
     }
     const [createUserWithEmailAndPassword, user, loading, error,] = useCreateUserWithEmailAndPassword(auth);
+    const [signInWithGoogle, user1, loading1, error1] = useSignInWithGoogle(auth);
     const [updateProfile, updating] = useUpdateProfile(auth);
     const [sendEmailVerification, sending] = useSendEmailVerification(auth);
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirm, setConfirm] = useState('')
-    const handleRegisterForm = e => {
+    const handleRegisterForm = async (e) => {
         e.preventDefault();
         if (password.length < 8) {
             toast.error('Password Length must be at least 8 characters', toastConfig)
@@ -33,27 +34,39 @@ const Register = () => {
             toast.error('Connfirm Password does not match', toastConfig)
             return
         }
-        createUserWithEmailAndPassword(email, password)
+        await createUserWithEmailAndPassword(email, password)
     }
     useEffect(() => {
-        if (user) {
-            async function updateName() {
-                await updateProfile({ displayName: name })
-            }
-            updateName()
-            async function sendVerification() {
+        if (!error?.message) {
+            const verify = async () => {
                 await sendEmailVerification()
                 toast.success('Email Verification sent', toastConfig)
             }
-            sendVerification()
+            verify()
         }
-    }, [user])
+    }, [error])
+    useEffect(() => {
+        if (user || user1) {
+            if (user?.user?.uid) {
+                async function updateName() {
+                    await updateProfile({ displayName: name })
+                }
+                updateName()
+            }
+            if (user1) {
+
+            }
+        }
+    }, [user, user1])
     useEffect(() => {
         if (error?.message) {
             toast.error(error.message, toastConfig)
         }
-    }, [error])
-    if (loading || updating || sending) {
+        else if (error1?.message) {
+            toast.error(error.message, toastConfig)
+        }
+    }, [error, error1])
+    if (loading || loading1 || updating || sending) {
         return <Loading></Loading>
     }
     return (
@@ -69,7 +82,7 @@ const Register = () => {
                 </form>
                 <p className="text-center">or</p>
                 <p className="text-center my-2">Already have an account? <Link to="/login" className='text-green-500'>Login</Link></p>
-                <button className="bg-blue-500 w-1/2 mx-auto block p-3">
+                <button className="bg-blue-500 w-1/2 mx-auto block p-3" onClick={() => signInWithGoogle()}>
                     <FcGoogle className="inline mx-2"></FcGoogle>
                     <span>Continue With Google</span>
                 </button>
